43470bb754baf87af9e7a387cb0f9be76b997955
Clombo <christo.lombard@live.co.za>
2021-03-08 18:52:40 +0200

    Build files included

---
+++ build/precache-manifest.be30e5f7b2615faf03c1ee85b57ccb7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2178d6181afad1188f79439c99d13b74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24aabbbeb34711a4cdf7",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0103b1cd5948b9bf6d0b",
+    "url": "./static/css/main.5d5a916e.chunk.css"
+  },
+  {
+    "revision": "24aabbbeb34711a4cdf7",
+    "url": "./static/js/2.25f46b14.chunk.js"
+  },
+  {
+    "revision": "1f3ec544f59db43d224e4cd99b0c3b1d",
+    "url": "./static/js/2.25f46b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0103b1cd5948b9bf6d0b",
+    "url": "./static/js/main.6adf2fec.chunk.js"
+  },
+  {
+    "revision": "855e77d94f4fff4d4166",
+    "url": "./static/js/runtime-main.3b5dfc25.js"
+  }
+]);
